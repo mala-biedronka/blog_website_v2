1e10b0935f091fadbd6a153f30b50a22df1f0eca
Iryna Predborska <iryna.predborska@gmail.com>
2020-05-01 13:54:26 +0200

    create home, about, contact, compose pages templates

--- app.js
+++ app.js
@@ -6,10 +6,26 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
+//Get requests
 app.get("/", function (req, res) {
     res.render("home");
 });
+
+app.get("/compose", function (req, res) {
+    res.render("compose");
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact");
+});
+
+//Post requests
 
 app.listen(3000, function () {
     console.log("The server is running on port 3000");
